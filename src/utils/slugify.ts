a65e79c3a0fd858bd6089d3744fb085fe17a6fdf
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-03-05 16:32:23 +1100

    Remove the Test Addition to Slugs

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -4,7 +4,7 @@ import { unemojify } from "node-emoji";
 slug.extend({ _: "-" });
 
 export function slugifyStr(str: string): string {
-  return slug(unemojify(str) + "_test_addition");
+  return slug(unemojify(str));
 }
 
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
