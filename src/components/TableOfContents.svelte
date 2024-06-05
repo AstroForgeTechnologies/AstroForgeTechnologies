ac201604c4b80e39cb3f38997ffc346d58428e00
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-06-05 17:26:00 +1000

    Table of Contents: Add Overflow Scrolling, Decrease Font Size

--- src/components/TableOfContents.svelte
+++ src/components/TableOfContents.svelte
@@ -53,15 +53,20 @@
 
 <svelte:window onscroll={onScroll} />
 
-<div class="flex w-72 items-start">
+<!-- 2 Px of Buffer Zone for the Border, 0.75 Rem of Buffer Zone for the scrollbar, 0.5 Rem of Buffer Zone for the Right -->
+<div
+  class="flex items-start"
+  style="width: calc(20rem + 2px + 0.75rem + 0.5rem)"
+>
   <div
-    class="sticky top-[150px] ml-16 flex w-72 min-w-0 flex-col gap-3 border-l-2 border-primary/50 py-4 pl-4"
+    class="sticky top-[150px] ml-16 flex min-w-0 flex-col gap-2 overflow-x-hidden overflow-y-scroll border-l-2 border-primary/50 py-4 pl-4 pr-2"
+    style="max-height: calc(100vh - 150px);"
   >
     {#each headings as heading}
       <a
         href={`#${heading.slug}`}
-        class={`w-72 min-w-0 transition-colors duration-200 ${closest === heading.slug ? "text-primary hover:text-accent" : "text-skin-base/50 hover:text-skin-base"}`}
-        style={`margin-left: calc(${heading.depth - 2} * 1rem)`}
+        class={`min-w-0 break-words transition-colors duration-200 ${closest === heading.slug ? "text-primary hover:text-accent" : "text-skin-base/50 hover:text-skin-base"} text-sm`}
+        style={`width: calc(15rem - ${heading.depth - 2} * 1rem); max-width: calc(15rem - ${heading.depth - 2} * 1rem); padding-left: calc(${heading.depth - 2} * 1rem);`}
         bind:this={elements[heading.slug]}
       >
         {heading.text}
