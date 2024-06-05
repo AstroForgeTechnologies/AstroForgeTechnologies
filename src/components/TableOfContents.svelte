b3a1bf1ea842e4a4063b41c751bb95eea07c255c
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2024-06-06 09:10:26 +1000

    Decrease ToC Width

--- src/components/TableOfContents.svelte
+++ src/components/TableOfContents.svelte
@@ -56,7 +56,7 @@
 <!-- 2 Px of Buffer Zone for the Border, 0.75 Rem of Buffer Zone for the scrollbar, 0.5 Rem of Buffer Zone for the Right -->
 <div
   class="flex items-start"
-  style="width: calc(20rem + 2px + 0.75rem + 0.5rem)"
+  style="width: calc(18rem + 2px + 0.75rem + 0.5rem)"
 >
   <div
     class="sticky top-[150px] ml-16 flex min-w-0 flex-col gap-2 overflow-x-hidden overflow-y-scroll border-l-2 border-primary/50 py-4 pl-4 pr-2"
@@ -66,7 +66,7 @@
       <a
         href={`#${heading.slug}`}
         class={`min-w-0 break-words transition-colors duration-200 ${closest === heading.slug ? "text-primary hover:text-accent" : "text-skin-base/50 hover:text-skin-base"} text-sm`}
-        style={`width: calc(15rem - ${heading.depth - 2} * 1rem); max-width: calc(15rem - ${heading.depth - 2} * 1rem); padding-left: calc(${heading.depth - 2} * 1rem);`}
+        style={`width: calc(13rem - ${heading.depth - 2} * 1rem); max-width: calc(13rem - ${heading.depth - 2} * 1rem); padding-left: calc(${heading.depth - 2} * 1rem);`}
         bind:this={elements[heading.slug]}
       >
         {heading.text}
